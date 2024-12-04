0a42596a00102b3a18dd42edd0973c23fae602c5
Hshefat <shefat.hasnain@gmail.com>
2024-12-04 14:18:36 +0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SidebarComponent } from './components/layouts/sidebar/sidebar.component';
+import { HeaderComponent } from './components/layouts/header/header.component';
+import { MasterComponent } from './components/master/master.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { MainComponent } from './components/main/main.component';
+import { DefaultComponent } from './components/default/default.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SidebarComponent,
+    HeaderComponent,
+    MasterComponent,
+    DashboardComponent,
+    MainComponent,
+    DefaultComponent
   ],
   imports: [
     BrowserModule,
